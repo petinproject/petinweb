c0b1e68f5f3a2acd15c402742558bb8f2ac6c5e7
petinproject <94122753+petinproject@users.noreply.github.com>
2024-09-10 19:20:08 -0500

    actualización de variables de entorno y ambientes

--- firebase-messaging-dev.js
+++ firebase-messaging-dev.js
@@ -2,16 +2,6 @@ importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-storage-compat.js");
 
-const firebaseConfigProd = {
-    apiKey: "AIzaSyAGWxqlKTss2SFE_jdq6aMLxD4EHx-E9oE",
-    authDomain: "petin-prod.firebaseapp.com",
-    projectId: "petin-prod",
-    storageBucket: "petin-prod.appspot.com",
-    messagingSenderId: "81894334028",
-    appId: "1:81894334028:web:8bd3054079c6d53418dee9",
-    measurementId: "G-2K1ZYNC00G"
-};
-
 const firebaseConfigDev = {
     apiKey: "AIzaSyC_wBmA-Sy9HzxJ2LSLFG7hlLcSzoXn7qE",
     authDomain: "petin-333318.firebaseapp.com",
@@ -22,15 +12,7 @@ const firebaseConfigDev = {
     measurementId: "G-9KSNF5RKHR"
 };
 
-firebase.initializeApp({
-    apiKey: "AIzaSyC_wBmA-Sy9HzxJ2LSLFG7hlLcSzoXn7qE",
-    authDomain: "petin-333318.firebaseapp.com",
-    projectId: "petin-333318",
-    storageBucket: "petin-333318.appspot.com",
-    messagingSenderId: "484491527318",
-    appId: "1:484491527318:web:38af7de134c56b968faeb7",
-    measurementId: "G-9KSNF5RKHR"
-});
+firebase.initializeApp(firebaseConfigDev);
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
 
--- firebase-messaging-prod.js
+++ firebase-messaging-prod.js
@@ -12,25 +12,7 @@ const firebaseConfigProd = {
     measurementId: "G-2K1ZYNC00G"
 };
 
-const firebaseConfigDev = {
-    apiKey: "AIzaSyC_wBmA-Sy9HzxJ2LSLFG7hlLcSzoXn7qE",
-    authDomain: "petin-333318.firebaseapp.com",
-    projectId: "petin-333318",
-    storageBucket: "petin-333318.appspot.com",
-    messagingSenderId: "484491527318",
-    appId: "1:484491527318:web:38af7de134c56b968faeb7",
-    measurementId: "G-9KSNF5RKHR"
-};
-
-firebase.initializeApp({
-    apiKey: "AIzaSyC_wBmA-Sy9HzxJ2LSLFG7hlLcSzoXn7qE",
-    authDomain: "petin-333318.firebaseapp.com",
-    projectId: "petin-333318",
-    storageBucket: "petin-333318.appspot.com",
-    messagingSenderId: "484491527318",
-    appId: "1:484491527318:web:38af7de134c56b968faeb7",
-    measurementId: "G-9KSNF5RKHR"
-});
+firebase.initializeApp(firebaseConfigProd);
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
 
